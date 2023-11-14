a330187c6d4918d068d609035070bc3278cf2a5d
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2023-11-14 21:12:48 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 const App = () => {
-  const [selectedShape, setSelectedShape] = useState('Square');
-  const [shapes, setShapes] = useState([]);
-
-  const handleShapeChange = (e) => {
-    setSelectedShape(e.target.value);
+ const [selectedShape, setSelectedShape] = useState("square");
+  const [shapeList, setShapeList] = useState([]);
+  const onButtonClick = () => {
+    setShapeList([...shapeList, selectedShape.toLowerCase()]);
   };
-
-  const addShape = () => {
-    const newShape = {
-      type: selectedShape,
-      id: new Date().getTime(), // Unique identifier (timestamp)
-    };
-    setShapes([...shapes, newShape]);
-  };
-
   return (
     <div id="main">
       <div id="shape-creator">
-        <select value={selectedShape} onChange={handleShapeChange}>
-          <option value="Square">Square</option>
-          <option value="Circle">Circle</option>
+        <select
+          value={selectedShape}
+          onChange={(e) => setSelectedShape(e.target.value)}
+        >
+          <option id="option-square">Square</option>
+          <option id="option-circle">Circle</option>
         </select>
-        <button onClick={addShape}>Add Shape</button>
+        <button onClick={onButtonClick}>Add Shape</button>
       </div>
       <div id="shapes-holder">
-        {shapes.map((shape) => (
-          <div key={shape.id} className={shape.type.toLowerCase()}>
-            {shape.type}
+        {shapeList.map((item, index) => (
+          <div key={index} className={item}>
+            {index}
           </div>
         ))}
       </div>
     </div>
   );
-};
+}
 
 export default App;
